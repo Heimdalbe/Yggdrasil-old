385694c63873ecfc52a5c12975c6ee82cf5a1d1b
nlersber <nick.lersberghe@student.hogent.be>
2020-05-27 23:52:02 +0200

    Added isAdmin check and check for args

--- commands/admin/ClearCommand.js
+++ commands/admin/ClearCommand.js
@@ -7,17 +7,53 @@ module.exports = class ClearCommand extends commando.Command {
       memberName: "clear",
       group: "admin",
       description: "Clears an amount of messages from a text channel",
-      args: [
-        {
-          key: "amount",
-          prompt: "How many messages would you like to clear?",
-          type: "number, string ('all')",
-        },
-      ],
+      argsCount: 1,
     });
   }
 
-  run(message, args){
-      console.log("Clear derp");
+  run(message, args) {
+    //Check if command user has Administrator permissions
+    let isAdmin = message.channel
+      .permissionsFor(message.member)
+      .has("ADMINISTRATOR", true);
+
+    if (!isAdmin) {
+      console.log("Not an admin");
+      message
+        .reply(
+          "You need Admin Permissions to run this command. Your command and this message will automatically self-destruct in 5 seconds."
+        )
+        .then((t) => {
+          t.delete(10000); //Delete reply
+          message.delete(10000); //Delete original command
+          return;
+        });
+    }
+
+    args=args.trim().split(" ")
+    console.log(!!args);
+    console.log(Array.isArray(args));
+    console.log(args.length);
+
+
+    if (args.length!==1) {
+      console.log("argcount fucked");
+      //Too few or too many
+      message
+        .reply(
+          "This command " +
+            (this.argsCount === 0 ? "only " : "") +
+            "needs to know how many messages should be deleted. Please provide a number or 'all' to clear all messages. \nYour command and this message will automatically self-destruct in 5 seconds."
+        )
+        .then((t) => {
+          t.delete(10000); //Delete reply
+          message.delete(10000); //Delete original command
+          return;
+        });
+    }
+
+    console.log("derp");
+    //Check if command was not sent in DM
+    if (message.channel) return;
   }
 };
--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const Discord = require("discord.js");
 const config = require("dotenv").config({ path: __dirname + "/.env" });
 const path = require("path");
 const commando = require("discord.js-commando");
-const settings = require("./settings.json");
 const fs = require("fs");
 
 const client = new commando.CommandoClient({
-  owner: "686344322089615387",//TODO change when bot ownership goes to admin account
-  commandPrefix: "h!",
+  owner: "686344322089615387", //TODO change when bot ownership goes to admin account
+  commandPrefix: ";",
   unknownCommandResponse: true,
 });
+
 module.exports = client;
 
-client.registry.registerDefaults();
-client.registry.registerCommandsIn(path.join(__dirname, "/commands"));
+client.registry
+  .registerDefaults() //Registers default stuff
+  .registerGroups([
+    //Registers the command groups
+    ["admin", "Administrator commands"],
+    ["user", "User commands"],
+  ])
+  .registerCommandsIn(path.join(__dirname, "commands")); //Registers where the command files can be find, separated by group in a folder with that groups name
 
 client.on("ready", () => {
-  console.log("Derp");
+  console.log("Ready to go");
 });
 
 client.login(process.env.TOKEN);
