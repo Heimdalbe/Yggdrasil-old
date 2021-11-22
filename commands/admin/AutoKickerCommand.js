3e3369e4944eba324d597544cf7786a399d166ec
Benny <benny.debock@student.hogent.be>
2021-11-22 18:29:07 +0100

    Initial creation autokickercommand

---
+++ commands/admin/AutoKickerCommand.js
@@ -0,0 +1,8 @@
+const commando = require("discord.js-commando");
+const helper = require("../../domain/CommandHelper");
+
+
+module.exports = class AutoKickerCommand extends commando.Command {
+    
+
+}
